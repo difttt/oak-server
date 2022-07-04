8c3f0149733b918521cfc0425503aa5570614a06
Li Smith <jinrishouji@sina.com>
2022-07-04 16:05:30 +0800

    send taskQueueTaskHashes as response

--- routes/api.js
+++ routes/api.js
@@ -4,19 +4,15 @@ var uuid = require("uuid");
 
 var oak_js_library = require("oak-js-library");
 var oakConstants = oak_js_library.oakConstants;
-var scheduler = new oak_js_library.Scheduler(oakConstants.OakChains.STUR);
-var observer = new oak_js_library.Observer(oakConstants.OakChains.STUR);
 
 router.get("/hash", async function (req, res, next) {
   //获取哈希
+  var observer = new oak_js_library.Observer(oakConstants.OakChains.STUR);
   const taskQueueTaskHashes = await observer.getAutomationTimeTaskQueue();
 
   res.status(200);
 
-  res.send({
-    success: true,
-    hashes: taskQueueTaskHashes,
-  });
+  res.send(taskQueueTaskHashes);
   //表示结束
   res.end();
 });
