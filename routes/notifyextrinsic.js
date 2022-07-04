69352f01e25c378a1ea31e678c1b048d6f9a1876
shiyivei <shiyivei@outlook.com>
2022-07-04 11:04:58 +0800

    create-message-and-times-params-api

--- routes/notifyextrinsic.js
+++ routes/notifyextrinsic.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 var uuid = require("uuid");
 const { Keyring } = require("@polkadot/keyring");
-const { ApiPromise, WsProvider } = require('@polkadot/api');
+const { ApiPromise, WsProvider } = require("@polkadot/api");
 
 var oak_js_library = require("oak-js-library");
 var oakConstants = oak_js_library.oakConstants;
@@ -22,48 +22,64 @@ router.get("/", async function (req, res, next) {
   res.end();
 });
 
-async function buildExtrinsic() {
-  // known mnemonic, well, now it is - don't use it for funds
+async function buildExtrinsic(times, msg) {
+  //MNEMONIC
   const MNEMONIC =
     "when flight maximum result trim wool legal emotion major photo alien emotion";
 
   // type: ed25519, ssFormat: 42 (all defaults)
-  const keyring = new Keyring({ type: 'sr25519', ss58Format: 51 });
+  const keyring = new Keyring({ type: "sr25519", ss58Format: 51 });
   const account_pair = keyring.createFromUri(MNEMONIC);
 
+  //1.address
   const senderAddress = "68H3fAKnJ6ahvzgcayZGoE7Rg7v828in5xjvouaQdginBgtu";
 
+  //2.providedID
   const providedID = uuid.v4();
   console.log("####### providedID", providedID);
 
-  const recurrer = new oak_js_library.Recurrer()
-  const recurrences = 3
-  // output is a 5-item array of unix timestamps
-  const timestamps = recurrer.getHourlyRecurringTimestamps(Date.now(), recurrences)
+  const recurrer = new oak_js_library.Recurrer();
+  // const recurrences = 3;
+  let recurrences = times;
+  console.log("####### recurrences", recurrences);
+  //3.timestamps
+  const timestamps = recurrer.getHourlyRecurringTimestamps(
+    Date.now(),
+    recurrences
+  );
 
   for (let index = 0; index < timestamps.length; index++) {
     const element = timestamps[index];
     timestamps[index] = Math.floor(element / 1000);
   }
 
-  const message = "Subscription Notification";
-  
+  //4.message
+  // const message = "Subscription Notification";
+  let message = msg;
+  console.log("###### message", message);
+
+  //extrinsic object
   let hex = await scheduler.buildScheduleNotifyExtrinsic(
     account_pair,
     providedID,
     timestamps,
-    message,
+    message
   );
 
-  return hex
+  return hex;
 }
 
 async function sendExtrinsic() {
+  //error handler
   const customErrorHandler = (result) => {
     console.error("###### error", JSON.stringify(result));
   };
+
+  //get hex
   let hex = await buildExtrinsic();
   console.log("###before send hex");
+
+  //convert tex and send extrinsic
   const txHash = await scheduler.sendExtrinsic(hex, customErrorHandler);
   console.log("###after send hex");
 
@@ -71,5 +87,24 @@ async function sendExtrinsic() {
 
   return txHash;
 }
+
+//post 请求，处理前端提供的数据
+router.post("/extrinsic", async function (req, res) {
+  //parse
+  const extrinsicHex = req.body;
+  console.log("parsed extrinsicHex:", extrinsicHex);
+
+  let hex = buildExtrinsic(extrinsicHex.times, extrinsicHex.message);
+  console.log("built hex", hex);
+
+  const txHash = await sendExtrinsic();
+
+  res.send({
+    success: true, //表示状态
+    txHash,
+  });
+
+  res.end();
+});
 
 module.exports = router;
