be89fff5731fe8c0934b02fd6a455d922cdcd6ec
Li Smith <jinrishouji@sina.com>
2022-07-04 16:03:43 +0800

    add event process

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ var logger = require("morgan");
 
 //引入路由
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 var apiRouter = require("./routes/api");
 var notifyRouter = require("./routes/notifyextrinsic");
+var eventRouter = require("./routes/event");
 
 var app = express();
 
@@ -24,9 +24,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/api", apiRouter);
 app.use("/notify", notifyRouter);
+app.use("/event", eventRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/event.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var router = express.Router();
+var graphql_request = require("graphql-request");
+
+router.get("/", async function (req, res, next) {
+
+  const query = graphql_request.gql`
+  {
+    events (first: 1, filter: {method: {equalTo:  "Notify"}}, orderBy: TIMESTAMP_DESC ) {
+        nodes {
+            id
+            module
+            method
+            data
+            timestamp
+        }
+    } 
+  } 
+  `
+
+  let data = await graphql_request.request('https://api.subquery.network/sq/OAK-Foundation/turing-staging-subql', query);
+  
+  let last_event = data.events.nodes[0];
+  console.log(last_event);
+
+  res.status(200);
+
+  res.send(last_event);
+  //表示结束
+  res.end();
+});
+
+module.exports = router;
